5537daeba35c85afc947c6ae2cabc7a6f1e3bac3
Delaksan-Sritharan <delaksan@gmail.com>
2025-09-18 12:38:31 +0530

    add NavBar component and update home page layout; define Resume and Feedback interfaces

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,28 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import NavBar from "~/components/ NavBar";
+import {resumes} from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "DelaProof" },
+    { name: "description", content: "Built by Dela, built for your career" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <NavBar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+
+    {resumes.map((resume)=>(
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+    ))}
+  </main>;
 }
