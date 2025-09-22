a613f2ef3bb6531a475635e60b8f613d9080f71f
Delaksan-Sritharan <delaksan@gmail.com>
2025-09-22 12:18:22 +0530

    enhance ResumeCard to display resume image in the card layout

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 
-const ResumeCard = ({resume:{id,companyName,jobTitle,feedback}}:{resume:Resume}) => {
+const ResumeCard = ({resume:{id,companyName,jobTitle,feedback,imagePath}}:{resume:Resume}) => {
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
@@ -17,6 +17,13 @@ const ResumeCard = ({resume:{id,companyName,jobTitle,feedback}}:{resume:Resume})
             <div className="flex-shrink-0">
                 <ScoreCircle score={feedback.overallScore}/>
             </div>
+            </div>
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full">
+                    <img src={imagePath}
+                    alt="resume"
+                    className="w-full h-[350px] max-sm:h-[200px] object-cover object-cover object-top"/>
+                </div>
             </div>
         </Link>
 
